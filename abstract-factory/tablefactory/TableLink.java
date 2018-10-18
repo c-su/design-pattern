23bd713f1e58b68a47a2ed0e19e0905c8f655053
c-su <m.y.like.music.10@gmail.com>
2018-10-18 22:40:13 +0900

    AbstractFactoryパターンの実装

---
+++ abstract-factory/tablefactory/TableLink.java
@@ -0,0 +1,16 @@
+package tablefactory;
+
+import factory.*;
+
+/**
+ * TableLink
+ */
+public class TableLink extends Link {
+  public TableLink(String caption, String url) {
+    super(caption, url);
+  }
+
+  public String makeHTML() {
+    return "<td><a href= \"" + url + "\">" + caption + "</a></td>\n";
+  }
+}
